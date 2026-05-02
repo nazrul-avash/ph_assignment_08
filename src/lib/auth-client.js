ca7d435c7a0d858fe6a9773f4f39746135355e47
nazrul-avash <nazrul.avash@gmail.com>
2026-05-03 03:50:40 +0600

    fixes bug

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL
+    baseURL: process.env.BETTER_AUTH_URL,
+    trustedOrigins: [
+    "https://ph-assignment-08-qoz5.vercel.app",
+    "http://localhost:3000", // keep for local dev
+  ],
 })
